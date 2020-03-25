a9833bfb382bf256e1ee8837480004386ed6233a
SakuMyl <myllymakisaku@gmail.com>
2020-03-25 18:42:28 +0200

    initialize structs properly

--- 5-server/main.c
+++ 5-server/main.c
@@ -95,37 +95,44 @@ int main(int argc, char **argv)
     if (!write_message(message, sockfd)) return 1;
     
     printf("Student number sent\n");
+    struct sockaddr_in own2;
+    socklen_t ownlen = sizeof(struct sockaddr);
+    if (getsockname(sockfd, (struct sockaddr *)&own2, &ownlen) < 0) {
+        perror("getsockname error");
+    }
+    short port = 7200;
     while (1) {
         char msg[MAXLINE];
+        printf("Before reading first message\n");
         if (!read_message(msg, sockfd)) return 1;
         printf("First message read %s\n", msg);
         if (strstr(msg, "OK") || strstr(msg, "FAIL")) {
             printf("%s", msg);
             return 0;
         }
-        int sockfd2;
-        int connfd;
+        int sockfd2, connfd;
         if (strcmp(msg, "MORE\n") == 0) {
+            struct sockaddr_in own, incoming;
             if ((sockfd2 = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
                 perror("socket error");
                 return 1;
             }
-            struct sockaddr_in own;
+            own.sin_family = AF_INET;
+            own.sin_addr.s_addr = INADDR_ANY;
+            own.sin_port = htons(port);
+            //own.sin_port = htons(7200);
             bind(sockfd2, (struct sockaddr *)&own, sizeof(struct sockaddr));
             listen(sockfd2, 10);
-            /*if (getsockname(sockfd2, (struct sockaddr *)&own, &ownlen) < 0) {
-                perror("getsockname error");
-                return 1;
-            }*/
-            strcpy(msg, "SERV ");
-            inet_ntop(AF_INET, &own.sin_addr, msg + 5, INET_ADDRSTRLEN);
-            if (sprintf(msg + strlen(msg), " %hu %c", ntohs(own.sin_port), '\n') < 2) {
+            char ipstring[INET_ADDRSTRLEN];
+            inet_ntop(AF_INET, &own2.sin_addr, ipstring, INET_ADDRSTRLEN);
+            if (sprintf(msg, "SERV %s %hu\n", ipstring, ntohs(own.sin_port)) < 2) {
                 perror("sprintf error");
                 return 1;
             }
             printf("Before writing SERV message: %s\n", msg);
             if (!write_message(msg, sockfd)) return 1;
-            struct sockaddr_in incoming;
+            incoming.sin_family = AF_INET;
+            incoming.sin_addr.s_addr = INADDR_ANY;
             socklen_t incominglen = sizeof(struct sockaddr);
             printf("Before accepting\n");
             if((connfd = accept(sockfd2, (struct sockaddr *)&incoming, &incominglen)) < 0) {
@@ -137,10 +144,12 @@ int main(int argc, char **argv)
         printf("Before reading byte count\n");
         if (read_bytes(connfd, &bytes, 4) < 4) return 1;
         bytes = ntohl(bytes);
+        printf("Bytes to send: %d", bytes);
         uint8_t msg_back[bytes];
         memset(msg_back, 'A', bytes);
         printf("Before writing bytes\n");
         if (write_bytes(connfd, msg_back, bytes) < bytes) return 1;
+        port++;
     }
     
     return 0;
